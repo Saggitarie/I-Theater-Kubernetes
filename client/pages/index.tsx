4eb5b5535d70d5d88404472fbf0fe761c4d37d4c
Saggitarie <saggitarie@gmail.com>
2021-01-02 22:41:56 +0900

    Design Done

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,19 +1,30 @@
 import Head from 'next/head';
 import React, { useEffect, useState } from "react";
 import axios from 'axios';
+import { copyFileSync } from 'fs';
 
 export default function Home(): React.ReactElement  {
-  const [movieName, setMovieName] = useState("");
-  const [movieList, setMovieList] = useState([]);
+  const [movieName, setMovieName] = useState<string>("");
+  const [movieList, setMovieList] = useState<Array<any>>([]);
 
   useEffect(() => {
-    console.log(movieList);
+    console.log(movieList)
   }, [movieList]);
+
+  useEffect(() => {
+    let movieList = "";
+    const fetchMovieList = async (): Promise<void> => {
+      movieList = await axios.get("/api/movielist");
+    }
+
+    console.log("Movielist", movieList)
+    
+    fetchMovieList();
+  }, [])
 
   function onMovieNameInput(event:React.ChangeEvent<HTMLInputElement>){
     let inputValue = (event.target as HTMLInputElement).value;
     setMovieName(inputValue);
-    console.log(movieName);
   }
 
   async function onClickWatchLater(event: React.MouseEvent<HTMLInputElement>){
@@ -21,7 +32,9 @@ export default function Home(): React.ReactElement  {
         movieName
     });
 
-    setMovieList(await axios.get("/api/movielist"));
+    let {data} = await axios.get("/api/movielist");
+
+    setMovieList(data);
   }
 
   return (
@@ -29,13 +42,43 @@ export default function Home(): React.ReactElement  {
       <Head>
         <title>I Theater</title>
         <link rel="icon" href="/favicon.ico" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link href="https://fonts.googleapis.com/css2?family=Unlock&display=swap" rel="stylesheet" />
       </Head>
-      <div >
-        <input value={movieName} onChange={onMovieNameInput}/>
+      <div className="wrapper">
+        <div className="search">
+          <div>
+            <h1 className="title">ITheater</h1>
+          </div>
+          <div>
+            <input className="input u-margin-bottom-medium" value={movieName} onChange={onMovieNameInput}/>
+          </div>
+          <div>
+            <button className="btn" onClick={onClickWatchLater}>Watch Later!</button>
+          </div>
+        </div>
+        <div>
+          {movieList.map((el:any) => {
+            if(movieList.length === 0){
+              return (<div></div>)
+            } else {
+              return (
+                <div className="result result__element">
+                  <div className="result__row--number">
+                    {el.id}
+                  </div>
+                  <div className="result__row--border"></div>
+                  <div className="result__row--title">
+                    {el.movieName}
+                  </div>
+                </div>
+              )
+            }
+          })}
+
+        </div>
       </div>
-      <div>
-        <button onClick={onClickWatchLater}>Watch Later!</button>
-      </div>
+
     </div>
   )
 }
