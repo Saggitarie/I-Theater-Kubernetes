3b90935108ed0e8854936403e634b477b4c3194b
Saggitarie <saggitarie@gmail.com>
2021-01-09 16:45:54 +0900

    Fix Prettier

--- client/.prettierrc.js
+++ client/.prettierrc.js
@@ -1,7 +1,4 @@
-// .prettierrc.js
 module.exports = {
-  // Change your rules accordingly to your coding style preferences.
-  // https://prettier.io/docs/en/options.html
   semi: true,
   trailingComma: 'es5',
   singleQuote: true,
